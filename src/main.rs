cc8c6e0cbcb46a591747c15412cfd8b58a66518e
Chris Overcash <covercash2@gmail.com>
2020-11-15 14:55:41 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(err) = msg.channel_id.say(&ctx.http, "hello").await {
+                println!("error sending message: {:?}", err);
+            }
+        } else {
+            println!("unexpected message: {:?}", msg);
+        }
+        if msg.content.contains("ultron") {
+            if let Err(err) = msg.channel_id.say(&ctx.http, "I am always listening").await {
+                println!("error sending message: {:?}", err);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("unable load env discord token");
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("unable to create client");
+
+    if let Err(err) = client.start().await {
+        println!("client error: {:?}", err);
+    }
+}
