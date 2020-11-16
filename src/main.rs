ca8c76930fe605918c9064ffadca8f1f17bc7c80
Chris Overcash <covercash2@gmail.com>
2020-11-16 13:23:21 -0600

    disable github and rustfmt

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,11 @@ use tokens::load_token;
 async fn main() {
     pretty_env_logger::init();
 
-    let github_token = load_token(tokens::GITHUB_TOKEN)
-        .expect("unable to load github token");
-    let discord_token = load_token(tokens::DISCORD_TOKEN)
-        .expect("unable to load discord token");
+    // TODO remove or do something with this
+    //let github_token = load_token(tokens::GITHUB_TOKEN).expect("unable to load github token");
+    let discord_token = load_token(tokens::DISCORD_TOKEN).expect("unable to load discord token");
 
     if let Err(err) = discord::run(discord_token).await {
-	error!("error running discord client: {:?}", err);
+        error!("error running discord client: {:?}", err);
     }
 }
