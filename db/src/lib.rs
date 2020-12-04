2b87c27c824663e86ec675e97dcd35f7b03c5fce
Chris Overcash <covercash2@gmail.com>
2020-12-03 18:19:48 -0600

    admin read and insert channel users

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -7,8 +7,8 @@ pub mod model;
 pub mod schema;
 
 use error::*;
-use model::BankAccount;
-use schema::bank_accounts::dsl::*;
+use model::{BankAccount, ChannelUser};
+use schema::{bank_accounts::dsl::*, channel_users::dsl::*};
 
 pub struct Db {
     connection: SqliteConnection,
@@ -28,19 +28,32 @@ impl Db {
     }
 
     pub fn insert_bank_account(&self, server: &u64, user: &u64, amount: &i32) -> Result<usize> {
-	diesel::insert_into(schema::bank_accounts::table)
+        diesel::insert_into(schema::bank_accounts::table)
             .values(&BankAccount::new(server, user, amount))
-	    .execute(&self.connection)
-	    .map_err(Into::into)
+            .execute(&self.connection)
+            .map_err(Into::into)
     }
 
     pub fn update_balance(&self, server: &u64, user: &u64, new_balance: &i32) -> Result<usize> {
-	let server = server.to_string();
-	let user = user.to_string();
-	diesel::update(bank_accounts.find((server, user)))
-	    .set(balance.eq(new_balance))
-	    .execute(&self.connection)
-	    .map_err(Into::into)
+        let server = server.to_string();
+        let user = user.to_string();
+        diesel::update(bank_accounts.find((server, user)))
+            .set(balance.eq(new_balance))
+            .execute(&self.connection)
+            .map_err(Into::into)
+    }
+
+    pub fn show_channel_users(&self) -> Result<Vec<ChannelUser>> {
+        channel_users
+            .load::<ChannelUser>(&self.connection)
+            .map_err(Into::into)
+    }
+
+    pub fn insert_channel_user(&self, server: &u64, channel: &u64, user: &u64) -> Result<usize> {
+        diesel::insert_into(schema::channel_users::table)
+            .values(&ChannelUser::new(server, channel, user))
+            .execute(&self.connection)
+            .map_err(Into::into)
     }
 }
 
