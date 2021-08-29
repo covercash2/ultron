110457ec846fa72df16d67eb304454e7697597d2
Chris Overcash <covercash2@gmail.com>
2021-08-29 18:36:40 -0500

    load account by server_id,user_id

--- rbatis/src/lib.rs
+++ rbatis/src/lib.rs
@@ -6,12 +6,14 @@ use std::fmt::Debug;
 use rbatis::{crud::CRUD, rbatis::Rbatis, Error as RbError};
 
 #[crud_table(table_name:bank_accounts)]
-#[derive(Clone, Debug)]
+#[derive(PartialEq, Clone, Debug)]
 pub struct Account {
     pub server_id: Option<String>,
     pub user_id: Option<String>,
     pub balance: Option<i32>,
 }
+
+type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
@@ -23,16 +25,29 @@ pub struct Db {
 }
 
 impl Db {
-    async fn open(url: &str) -> Result<Db, Error> {
+    async fn open(url: &str) -> Result<Db> {
         let rb = Rbatis::new();
         rb.link(url).await.map_err(|err| Error::Rbatis(err))?;
 
         return Ok(Db { connection: rb });
     }
 
-    async fn all_accounts(&self) -> Result<Vec<Account>, Error> {
+    async fn all_accounts(&self) -> Result<Vec<Account>> {
         self.connection
             .fetch_list()
+            .await
+            .map_err(|err| Error::Rbatis(err))
+    }
+
+    async fn account(&self, server_id: &u64, user_id: &u64) -> Result<Account> {
+        let query = self
+            .connection
+            .new_wrapper()
+            .eq("server_id", server_id.to_string())
+            .eq("user_id", user_id.to_string());
+
+        self.connection
+            .fetch_by_wrapper(&query)
             .await
             .map_err(|err| Error::Rbatis(err))
     }
@@ -60,9 +75,14 @@ mod tests {
     fn it_works() {
         let db = open_test_db();
         let accounts = async_run!(db.all_accounts()).expect("unable to load accounts");
-        
+
         accounts.iter().for_each(|account| {
             log::info!("{:?} {:?}", account.user_id, account.balance);
+            let other_account = async_run!(db.account(
+                &account.server_id.as_ref().unwrap().parse::<u64>().unwrap(),
+                &account.user_id.as_ref().unwrap().parse::<u64>().unwrap(),
+            )).expect("unable to load other account");
+            assert_eq!(account, &other_account);
         });
 
         assert_eq!(2 + 2, 4);
