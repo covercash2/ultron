74debcdf630d0c9b085cca0f47bccc7d8c52247a
Chris Overcash <covercash2@gmail.com>
2025-10-19 14:26:54 -0500

    feat: add support for Grafana webhooks

---
+++ core/src/grafana/testdata/mod.rs
@@ -0,0 +1,3 @@
+/// an example from the upstream docs:
+/// <https://grafana.com/docs/grafana/latest/alerting/configure-notifications/manage-contact-points/integrations/webhook-notifier/#default-json-payload>
+pub const GRAFANA_EXAMPLE: &str = include_str!("./grafana_example.json");
